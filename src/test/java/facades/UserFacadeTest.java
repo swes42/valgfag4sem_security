5441f2337f15f1759e6083cbca1ba9ab86a5a218
Mia Simone <cph-md291@cphbusiness.dk>
2021-04-28 14:50:18 +0200

    I dont know what im stashing

--- src/test/java/facades/UserFacadeTest.java
+++ src/test/java/facades/UserFacadeTest.java
@@ -40,9 +40,8 @@ public class UserFacadeTest {
         EntityManager em = emf.createEntityManager();
         try {
             em.getTransaction().begin();
-            em.createQuery("DELETE FROM Post").executeUpdate();
-            em.createQuery("DELETE FROM User").executeUpdate();
-            em.createQuery("DELETE FROM Role").executeUpdate();
+            em.createQuery("delete from User").executeUpdate();
+            em.createQuery("delete from Role").executeUpdate();
             em.persist(new User("Username1", "password1"));
             em.persist(new User("Username2", "password2"));
             em.getTransaction().commit();
